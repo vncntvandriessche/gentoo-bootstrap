24c4ea9530329923a4a96af5007be62c7a2bf1f0
Vincent Van Driessche <vncntvandriessche@gmail.com>
2013-09-30 17:06:21 +0200

    Adds the chroot script

---
+++ chroot/configure_portage.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function failed {
+  echo $1;
+  exit 1;
+}
+
+source /etc/profile || failed 'Failed to update to valid source.';
+
+emerge-webrsync || failed 'Failed installing portage snapshot.';
+
+emerge --sync --quiet || failed 'Failed updating the portage tree.';
+
+# Search for the gnome subprofile number
+gnome_profile=$(eselect profile list | sed -n 's/\[\([0-9]*\)\].*\/desktop\/gnome/\1/p') || failed 'Failed displaying eselect list';
+# Select that profile
+eselect profile set $gnome_profile
