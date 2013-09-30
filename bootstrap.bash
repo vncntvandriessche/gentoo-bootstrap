bad79e247894e36bca96d512d7736cf0936d2ba7
Vincent Van Driessche <vncntvandriessche@gmail.com>
2013-09-30 19:42:14 +0200

    Elaborates more!

--- bootstrap.bash
+++ bootstrap.bash
@@ -19,13 +19,21 @@ done;
 # Get variables from configuration files
 apply_file_type $CONFIG_FILES;
 
-# TODO: Elaborate more on the scripts functionality
 message="\
 --------------------------------------------------------------------------------\n\
 The system with ip |$ip_address| will be bootstrapped. During this process\n\
 it will create a partitioning scheme, create filesystems,... until the\n\
 installation is ready to be continued by a real configuration automation\n\
-framework like 'Puppet' or 'Chef'.\n\
+framework like 'Puppet' or 'Chef'.\n\n\
+Variables and function loaded:\n\
+------------------------------\n\
+Ip address target host: ${ip_address}.\n\
+General file location: ${file_location}.\n\
+Files specified:\n\
+  - ${stage}.\n\
+  - ${contents}.\n\
+  - ${digest}.\n\
+Downloader and args to be used: ${downloader}.\n\
 --------------------------------------------------------------------------------\
 ";
 
